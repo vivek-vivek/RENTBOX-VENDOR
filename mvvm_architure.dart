b6c750c2a0d9d0c1399311fd39c4b70805d2f6d5
vivek-vivek <codemygallery@gmail.com>
2023-02-07 12:31:17 +0530

    initial commit

---
+++ mvvm_architure.dart
@@ -0,0 +1,24 @@
+/*
+data
+This directory will hold all the network & local DB-related classes.
+
+models
+This directory will hold all the model classes for the API response and for clean architecture create inner directories for each API response model class.
+
+repository
+This directory will hold all the repository classes used in the project for communicating with ViewModel and for clean architecture create an inner directory for each module of your project.
+
+res
+This directory will hold all the classes related to the colors, style & strings files.
+
+utils
+This will hold all the Utility classes for the project.
+
+View
+this directory will hold all the view-related classes with subdirectory as per the module & widget as well for the project.
+
+view_model
+this directory will hold all the ViewModel-related classes with subdirectory if required.
+
+
+*/
